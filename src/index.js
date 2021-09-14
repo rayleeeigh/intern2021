15eeafd03d87e79d729a435a9f7a9a2c98376626
rayleeeigh <raylxylem@gmail.com>
2021-09-14 09:29:31 +0800

    reset password ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/resetpassword';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react"
 
