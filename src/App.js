af9f39fe7df0139b69ba537a8054c698a04ef835
rayleeeigh <raylxylem@gmail.com>
2021-09-15 10:03:03 +0800

    hehe

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from "react";
-import "./App.css";
-import Formpage from "./components/Form";
-import Login from "./components/Login";
-import ResetPass from "./components/resetpassword";
-
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/form" component={Formpage} />
-          <Route path="/login" component={ResetPass} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+    </div>
+  );
 }
 
 export default App;
