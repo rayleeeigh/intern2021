9894db6a000cadf587831df20ce00ebbee454e51
arnani2212 <arnanplanco@gmail.com>
2021-09-18 10:18:27 +0800

    [UPDATE][ARNAN]Login Validation added

--- src/components/Validation.js
+++ src/components/Validation.js
@@ -1,8 +1,13 @@
 const Validation = (user) => {
   // var cnt = 0;
   let errors = {};
+  let re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   if (!user.email) {
     errors.email = "Email is required.";
+  } else if (!re.test(user.email)) {
+    errors.email = "Wrong Email Format";
   }
   if (!user.password) {
     errors.password = "Password is required";
